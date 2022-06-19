ce70e8e9c5de14cd2b166258742f2c1b36de0e6e
AydarGaliev <3ajimka@gmail.com>
2022-06-19 23:06:42 +0300

    Начало решение контрольной работы. Создание программы, создание гит игнор

---
+++ Test_1/Program.cs
@@ -0,0 +1,6 @@
+﻿string[] array = new string[]
+{"hello", "2", "world", ":-)",
+ "1234", "1567", "-2", "computer science",
+ "Russia", "Denmark", "Kazan"};
+string[] newArray = new string[array.Length];
+int size = 3;
